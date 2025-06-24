fddef75074b758358bb9548fe656c01461c424ea
Seva Zaikov <seva.zaikov@gmail.com>
2025-06-23 17:07:24 -0700

    Initial Commit

---
+++ Streakosaur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Streakosaur
+//
+//  Created by Vsevolod Zaikov on 6/23/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Streakosaur/StreakosaurApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakosaurApp.swift
+//  Streakosaur
+//
+//  Created by Vsevolod Zaikov on 6/23/2025.
+//
+
+import SwiftUI
+
+@main
+struct StreakosaurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
